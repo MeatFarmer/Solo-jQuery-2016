fcf36e895caa91928f67f4be1f35a1f4d50e2bbe
Joseph Barsness <joseph.barsness@gmail.com>
2016-12-05 15:33:03 -0600

    final commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+console.log('js start');
+$( document ).ready(function() {
+    console.log('jq start');
+    $(".color-button").on("click", function() {
+      console.log('It is fairly clear I have no idea what I am doing.');
+  });
+}); // end document.ready
+
+// $(".practice").on("click", function () {
+// var outputText = '';
+// outputText += $('.domInfo').append('<p class="party">do you work?</p>');
+// });
+
+// $(".practice").on("click", function () {
+// console.log('button click works');
+// });
